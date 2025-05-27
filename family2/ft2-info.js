95effa4ffff4e3dcd68ecbdc3ebe022ae1d12e3c
Tessa <t.a.hope202@gmail.com>
2025-05-27 18:57:17 -0400

    Create ft2-info.js

---
+++ family2/ft2-info.js
@@ -0,0 +1,58 @@
+const familyData = {
+  "nodes": [
+  {
+    "key": 1,
+    "name": "Name 1"
+  },
+  {
+    "key": 2,
+    "name": "Name 2"
+  },
+  {
+    "key": 3,
+    "name": "Name 3"
+  },
+  {
+    "key": 4,
+    "name": "Name 4"
+  },
+  {
+    "key": 5,
+    "name": "Name 5"
+  },
+  {
+    "key": 6,
+    "name": "Name 6"
+  },
+  {
+    "key": 7,
+    "name": "Name 7"
+  },
+],
+  "links": [
+  {
+    "from": 1,
+    "to": 2
+  },
+  {
+    "from": 1,
+    "to": 3
+  },
+  {
+    "from": 2,
+    "to": 4
+  },
+  {
+    "from": 2,
+    "to": 5
+  },
+  {
+    "from": 3,
+    "to": 6
+  },
+  {
+    "from": 4,
+    "to": 7
+  },
+  ],
+};
